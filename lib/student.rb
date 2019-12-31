cef492f72ed577f4ec8e705e3b348a778ef2d93b
James Lenahan <james.k.lenahan@gmail.com>
2019-12-31 16:04:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,13 +31,12 @@ class Student
       INSERT INTO students (name, grade)
         VALUES (?, ?)
     SQL
-    
+
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
-end
